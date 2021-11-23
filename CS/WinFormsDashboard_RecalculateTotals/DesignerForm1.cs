80e3d082a89eebf121e2caae181e1f13a37beeae
Sergey Nakhankov <sergey.nakhankov@devexpress.com>
2021-11-23 15:15:55 +0300

    Remove unnecessary usings

--- CS/WinFormsDashboard_RecalculateTotals/DesignerForm1.cs
+++ CS/WinFormsDashboard_RecalculateTotals/DesignerForm1.cs
@@ -1,22 +1,4 @@
-﻿
-using DevExpress.DashboardCommon;
-using DevExpress.DashboardCommon.Localization;
-using DevExpress.DashboardCommon.Native;
-using DevExpress.DashboardCommon.Viewer;
-using DevExpress.DashboardCommon.ViewerData;
-using DevExpress.DashboardCommon.ViewModel;
-using DevExpress.DashboardExport;
-using DevExpress.DashboardWin;
-using DevExpress.XtraGrid;
-using DevExpress.XtraGrid.Columns;
-using DevExpress.XtraGrid.Views.Grid;
-using DevExpress.XtraReports.UI;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-
-namespace WinFormsDashboard_RecalculateTotals {
+﻿namespace WinFormsDashboard_RecalculateTotals {
     public partial class DesignerForm1 : DevExpress.XtraBars.Ribbon.RibbonForm {
         public DesignerForm1() {
             InitializeComponent();
